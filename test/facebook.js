c0d4c01f9609f8b9a3fb0456e65c7e8868258a47
PradatiusD <danielprada2012@gmail.com>
2014-08-27 11:51:19 -0400

    Added sticky footer, facebook link on page, and some more styling

--- test/facebook.js
+++ test/facebook.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var FB  = require('fb');
-var env = require('./../config/env/development').facebook;
+// var env = require('./../config/env/development').facebook;
 
 
 /*
@@ -10,18 +10,19 @@ var env = require('./../config/env/development').facebook;
  * ID: 1458319214390728
  */
 
-FB.api('/oauth/access_token?client_id='+env.clientID+'&client_secret='+env.clientSecret+'&grant_type=client_credentials', function(response) {
-console.log(response);
-});
+// FB.api('/oauth/access_token?client_id='+env.clientID+'&client_secret='+env.clientSecret+'&grant_type=client_credentials', function(response) {
+// console.log(response);
+// });
 
-// FB.setAccessToken('CAACEdEose0cBAL7v1ZA4WFDnGNwJUQbfAQ8HcCqZCkyBOdh6xsemB0xXaxdA7IGAcZAzJL1pi1MUCLllLCiWL5cGeQZCrOIhCFedtUg9Q9nurgGTK1sGNsbdLwqDk4qpncKH2lcKBWAMu7bq1wFAQ3ZBccmPljbmpyuAu7CSAb9y3VfMvCraNf4GjKJTEbEk6k8jX4Vwi9OoRZBlJLLOVZC');
+// Need group email
+FB.setAccessToken('CAACEdEose0cBAJhX5RBYNZB1aUKuaW9dQLfOZCYCE11URMwXkMlGFngEmNnIzTPArIK8o8x9J1GHfZAUNJKxu8oXfdbsR51rSnZCZB2xPE167pK9Sn0dJEjZAaiUM2IrzhhrLoEFAyGAuZAJJ65rqRZCxJqu76w3KVLbhU7kwuErZBZACJPIO7lHYrkdJjw1FiHvMBF1v10VVwDRDrwweP4ggd');
 
 // Use http://lookup-id.com/ for facebook id lookup
 
-// FB.api('1458319214390728/feed', function (res) {
-//   if(!res || res.error) {
-//     console.log(!res ? 'error occurred' : res.error);
-//     return;
-//   }
-//   console.log(JSON.stringify(res, null, 4));
-// });
+FB.api('243109099041731/feed', function (res) {
+  if (!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+  console.log(JSON.stringify(res, null, 4));
+});
