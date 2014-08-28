6d34b50d782304bd3e158a1836a34752609c1873
PradatiusD <danielprada2012@gmail.com>
2014-08-28 14:35:09 -0400

    Mobile responsiveness and navigation fixes

--- packages/system/public/controllers/header.js
+++ packages/system/public/controllers/header.js
@@ -22,7 +22,13 @@ angular.module('mean.system').controller('HeaderController', ['$scope', '$rootSc
     // Query server for menus and check permissions
     queryMenu('main', defaultMainMenu);
 
-    $scope.isCollapsed = false;
+    $scope.navCollapsed = false;
+
+    $scope.changeCollapse = function () {
+      if (window.innerWidth <= 767){
+        $scope.navbarCollapsed = !$scope.navbarCollapsed;
+      }
+    };
 
     $rootScope.$on('loggedin', function() {
 
